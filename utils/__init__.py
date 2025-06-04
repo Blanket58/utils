450f2b7c4da43d821aad8d2b74e27c87ed1a2c59
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2025-06-04 11:55:28 +0800

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,27 @@
+__author__ = 'BI/GANG'
+__date__ = '2024-08-16'
+__doc__ = r"""
+
+ __  __  ______  ______   __       ____
+/\ \/\ \/\__  _\/\__  _\ /\ \     /\  _`\
+\ \ \ \ \/_/\ \/\/_/\ \/ \ \ \    \ \,\L\_\
+ \ \ \ \ \ \ \ \   \ \ \  \ \ \  __\/_\__ \
+  \ \ \_\ \ \ \ \   \_\ \__\ \ \L\ \ /\ \L\ \
+   \ \_____\ \ \_\  /\_____\\ \____/ \ `\____\
+    \/_____/  \/_/  \/_____/ \/___/   \/_____/
+
+utils工具包 - 提供了部署Python自动化任务过程中可能会使用到的一些功能
+===================================================================
+
+主要功能
+-------
+
+  - utils.db 提供连接数据库进行SQL执行、获取结果、上传数据的功能
+  - utils.decorator 提供函数异常自动重试、函数计时、任务管理装饰器
+  - utils.tool 提供生成日志器的工厂类、分案时保证案量与总金额同时均分的功能、LGBModel导出为规则、DingDing群聊机器人推送消息
+
+"""
+
+from .db import OdpsConnector, parse_tables_from_sql, parse_tables_from_sqls, check_tables_privileges
+from .decorator import TaskHandler, retry, timer
+from .tool import LoggerFactory, balance, export_to_rule, dingbot, find_all_values_in_json
