9e23ae85746bc5ff6050dc38cb444df749eb900e
Blanket58 <39766189+Blanket58@users.noreply.github.com>
2026-01-05 09:51:35 +0800

    Add PDF to PNG conversion tool in utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -16,10 +16,10 @@ utils工具包 - 提供了部署Python自动化任务过程中可能会使用到
 
   - utils.db 提供连接数据库进行SQL执行、获取结果、上传数据的功能
   - utils.decorator 提供函数异常自动重试、函数计时、任务管理装饰器
-  - utils.tool 提供生成日志器的工厂类、分案时保证案量与总金额同时均分的功能、LGBModel导出为规则、DingDing群聊机器人推送消息
+  - utils.tool 提供生成日志器的工厂类、分案时保证案量与总金额同时均分的功能、LGBModel导出为规则、DingDing群聊机器人推送消息、PDF转PNG工具
 
 """
 
 from .db import OdpsConnector, parse_tables_from_sql, parse_tables_from_sqls, check_tables_privileges
 from .decorator import TaskHandler, retry, timer
-from .tool import LoggerFactory, balance, export_to_rule, dingbot, find_all_values_in_json
+from .tool import LoggerFactory, balance, export_to_rule, dingbot, find_all_values_in_json, pdf2png
